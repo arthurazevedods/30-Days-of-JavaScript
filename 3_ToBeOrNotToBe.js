738e77a683a875cba5a61d0cc4149563c84e56e0
Arthur Silva <arthurazevedods@gmail.com>
2024-01-10 11:28:08 -0300

    better ToBeOrNotToBe

--- 3_ToBeOrNotToBe.js
+++ 3_ToBeOrNotToBe.js
@@ -3,6 +3,29 @@
  * @return {Object}
  */
 let expect = function(val) {
+    function toBe(value) {
+        if (val !== value){
+            throw new Error("Not Equal");
+        } 
+        else{
+            return true;
+        } 
+    }
+    function notToBe(value){
+        if (val === value){
+            throw new Error("Equal");
+        } 
+        else{
+            return true;
+        }   
+    }
+    return {
+        toBe: toBe,
+        notToBe: notToBe
+    }
+};
+/*
+let expect = function(val) {
 
     return {
         toBe: (value) => {
@@ -23,3 +46,4 @@ let expect = function(val) {
         }
     }
 };
+*/
